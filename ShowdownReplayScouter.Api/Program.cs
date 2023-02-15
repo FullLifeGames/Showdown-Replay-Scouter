9734c61637532cf3941f12050a020d47827d29af
FullLifeGames <FullLifeGames@users.noreply.github.com>
2023-02-15 21:49:53 +0100

    Fix: Api produces typings

--- ShowdownReplayScouter.Api/Program.cs
+++ ShowdownReplayScouter.Api/Program.cs
@@ -5,9 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers().AddNewtonsoftJson();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
 
@@ -24,8 +22,9 @@ app.UseCors(builder => builder
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseOpenApi(); // serve OpenAPI/Swagger documents
+    app.UseSwaggerUi3(); // serve Swagger UI
+    app.UseReDoc(); // serve ReDoc UI
 //}
 
 if (!app.Environment.IsDevelopment())
